a279a29f94b4c9aeda1906b09d1ac58f28f81685
Luke <Lukebone@users.noreply.github.com>
2020-11-17 21:39:28 -0600

    Initial

---
+++ .qawolf/myFirstTest2.test.js
@@ -0,0 +1,25 @@
+const qawolf = require("qawolf");
+
+let browser;
+let context;
+
+beforeAll(async () => {
+  browser = await qawolf.launch();
+  context = await browser.newContext();
+  await qawolf.register(context);
+});
+
+afterAll(async () => {
+  await qawolf.stopVideos();
+  await browser.close();
+});
+
+test("myFirstTest2", async () => {
+  const page = await context.newPage();
+  await page.goto("http://todomvc.com/examples/react/", { waitUntil: "domcontentloaded" });
+  await page.click(".new-todo");
+  await page.fill(".new-todo", "eat pizza");
+  await page.press(".new-todo", "Enter");
+  await page.click(".toggle");
+  await page.click(".destroy");
+});
